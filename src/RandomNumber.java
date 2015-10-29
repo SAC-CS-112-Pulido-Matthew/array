210bab5515790a0d3757943dfad7073d2960ebfe
SAC-CS-112-Pulido-Matthew <matthew.n.pulido@gmail.com>
2015-10-28 19:59:00 -0700

    Array

--- src/RandomNumber.java
+++ src/RandomNumber.java
@@ -1,10 +1,23 @@
 
 public class RandomNumber
 {
+	public int GetANumber()
+	{
+		int number;
+		number = (int)(Math.random() * 10 ) + 1;
+		return number;
+	}
+	
 	public int GetANumber(int high)
 	{
 		int number;
 		number = (int)(Math.random() * high ) + 1;
 		return number;
+	}
+	
+	public int GetANumber(int high, int low)
+	{
+		int range = (high - low) + 1;     
+		return (int)(Math.random() * range) + low;
 	}
 }
